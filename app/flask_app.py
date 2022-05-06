3428d3f30810622ea3ab1d01523fe3ef017cdd24
Rui Ma <xjmarui@gmail.com>
2022-05-06 20:11:37 +0800

    修改 json response 异常的问题 create_app --> method chaining

--- app/flask_app.py
+++ app/flask_app.py
@@ -2,93 +2,105 @@ import logging
 from flask import Flask
 
 
+class App(Flask):
+
+    def load_config(self):
+        """
+        载入配置信息
+        :param self: flask instance
+        :return self: flask instance
+        """
+        from .config import Config
+        self.config.from_object(Config)
+
+        return self
+
+    def init_logger(self):
+        """
+        注册 logger
+        :param self: flask instance
+        :return self: flask instance
+        """
+        # logger 单例
+        logger = logging.getLogger(self.name)
+
+        for log_conf in self.config.get('LOGGING', []):
+            log_level = log_conf.get('level', logging.WARNING)
+
+            # debug 级别开启 debug
+            if log_level < logging.WARNING:
+                self.debug = True
+
+            # instance handler
+            handler = getattr(logging, log_conf.get('handler'))(
+                *log_conf.get('handler_args', [])
+            )
+
+            handler.setLevel(log_level)
+
+            log_format = logging.Formatter(log_conf.get('format'))
+            handler.setFormatter(log_format)
+
+            # register logger
+            logger.addHandler(handler)
+
+        return self
+
+    def init_app(self):
+        return self
+
+    def load_common_middlewares(self):
+        """
+        给 app 注册中间件
+        :param self: flask instance
+        :return self: flask instance
+        """
+        from app.middlewares import common_middlewares
+
+        for middleware in [middleware() for middleware in common_middlewares]:
+            self.before_request(middleware.before_request)
+            self.after_request(middleware.after_request)
+
+            if hasattr(middleware, 'error_handler'):
+                self.errorhandler(Exception)(middleware.error_handler)
+
+        return self
+
+    def load_blueprints(self):
+        """
+        注册 blueprint
+        :param self: flask instance
+        :return self: flask instance
+        """
+        from app.blueprints import bps
+
+        for url_prefix, bp in bps.items():
+            self.register_blueprint(bp, url_prefix=url_prefix)
+        return self
+
+    def load_api(self):
+        """
+        1. 注册 api namespace 下的 blueprint
+        2. 注册 restful API
+        :param self: flask instance
+        :return self: flask instance
+        """
+        from app.apis import apis
+        for url_prefix, api in apis.items():
+            self.register_blueprint(api, url_prefix='/api' + url_prefix)
+
+        return self
+
+
 def create_app():
-    app = Flask(__name__.split('.')[0])
-    register_config(app)
-    register_logger(app)
-    register_middleware(app)
-    register_blueprint(app)
-    register_api(app)
+    app = App(__name__.split('.')[0])
+
+    (
+        app.load_config()
+        .init_app()
+        .load_common_middlewares()
+        .load_blueprints()
+        .load_api()
+    )
 
     return app
-
-
-def register_logger(app):
-    """
-    注册 logger
-    :param app:
-    :return:  none
-    """
-
-    # logger 单例
-    logger = logging.getLogger(app.name)
-
-    for log_conf in app.config.get('LOGGING', []):
-        log_level = log_conf.get('level', logging.WARNING)
-
-        # debug 级别开启 debug
-        if log_level < logging.WARNING:
-            app.debug = True
-
-        # instance handler
-        handler = getattr(logging, log_conf.get('handler'))(
-            *log_conf.get('handler_args', [])
-        )
-
-        handler.setLevel(log_level)
-
-        log_format = logging.Formatter(log_conf.get('format'))
-        handler.setFormatter(log_format)
-
-        # register logger
-        logger.addHandler(handler)
-
-
-def register_middleware(app):
-    """
-    给 app 注册中间件
-    :param app: flask instance
-    :return: none
-    """
-    from app.middlewares import common_middlewares
-
-    for middleware in [middleware() for middleware in common_middlewares]:
-        app.before_request(middleware.before_request)
-        app.after_request(middleware.after_request)
-
-        if hasattr(middleware, 'error_handler'):
-            app.errorhandler(Exception)(middleware.error_handler)
-
-
-def register_blueprint(app):
-    """
-    注册 blueprint
-    :param app: flask instance
-    :return: none
-    """
-    from app.blueprints import bps
-
-    for url_prefix, bp in bps.items():
-        app.register_blueprint(bp, url_prefix=url_prefix)
-
-
-def register_api(app):
-    """
-    1. 注册 api namespace 下的 blueprint
-    2. 注册 restful API
-    :param app: flask instance
-    :return: none
-    """
-    from app.apis import apis
-    for url_prefix, api in apis.items():
-        app.register_blueprint(api, url_prefix='/api' + url_prefix)
-
-
-def register_config(app):
-    """
-    注册配置信息
-    :param app: flask instance
-    :return: none
-    """
-    from .config import Config
-    app.config.from_object(Config)
--- app/middlewares/json.py
+++ app/middlewares/json.py
@@ -8,10 +8,10 @@ class JsonMiddleware(BaseMiddleware):
     def after_request(self, response):
         # 对返回结果强制进行转为 json 结果
         data = response.get_json(force=True)
-        if 'data' not in data:
+        if not isinstance(data, (dict,)) or 'data' not in data:
             return jsonify({
                 'code': 0,
-                'data': response.get_json(force=True)
+                'data': data
             })
 
         # data 存在，则不进行处理
